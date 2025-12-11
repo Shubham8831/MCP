72f31c89b37f471212d2cd0577fbfccaee6b3acb
Shubham8831 <shubuamsingh2002@gmail.com>
2025-12-11 16:22:36 +0530

    basic mcp server made

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+from fastmcp import FastMCP
+
+#create a fastmcp server instance
+mcp = FastMCP("Demo server")
+
+@mcp.tool
+def roll_dice(n_dice: int = 1) -> list[int]:
+    return [random.randint(1,6) for _ in range(n_dice)]
+
+@mcp.tool
+def add_nimbers(a:float, b:float)-> float:
+    return a+b
+
+if __name__=="__main__":
+    mcp.run() # to run the mcp server
